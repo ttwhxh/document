24d72a8a62332b5a04c8183a0da84709c77e7072
ttwhxh <46936039+ttwhxh@users.noreply.github.com>
2019-02-15 15:24:02 -0600

    Completed Challenge
    Added .txt files, .cs file, and other project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Document
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Document\n");
+
+            string name;
+            Console.Write("Please enter a name for the document: ");
+            name = Console.ReadLine();
+            Console.WriteLine("");
+
+            string content;
+            Console.Write("Please enter the content within this document: ");
+           content = Console.ReadLine();
+            Console.WriteLine("");
+
+            try
+            {
+                StreamWriter sw = new StreamWriter(name + ".txt");
+                sw.WriteLine(content);
+                sw.Close();
+                Console.WriteLine("{0} has been saved.", name + ".txt");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }   
+     }
+ }
+
